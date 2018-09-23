4b3c58f334794c5ce10737e5e4685c0ccc465751
jiaoguanwen <jiaoguanwen@csii.com.cn>
2018-09-23 21:23:27 +0800

    feat: add ejs pug tpl

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 const Koa = require('koa')
 const app = new Koa()
-const { normal } = require('./tpl')
+const { htmlTpl, ejsTpl, pugTpl } = require('./tpl')
+// const ejs = require('ejs')
+const pug = require('pug')
 app.use(async (ctx, next) => {
   ctx.type = 'text/html; charset=utf-8'
-  ctx.body = normal
+  // ctx.body = htmlTpl
+  /* ctx.body = ejs.render(ejsTpl, {
+    you: 'Luke',
+    me: 'Jiaoguanwen'
+  }) */
+  ctx.body = pug.render(pugTpl, {
+    you: 'Luke',
+    me: 'Jiaoguanwen'
+  })
 })
 app.listen(2333)
