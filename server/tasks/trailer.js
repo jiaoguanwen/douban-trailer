7ae43bf7fc8b7713a2b36bb1bdc1dffaaf76f813
jiaoguanwen <jiaoguanwen@163.com>
2018-11-15 13:57:30 +0800

    can get video url

--- server/tasks/trailer.js
+++ server/tasks/trailer.js
@@ -1,27 +1,26 @@
 const cp = require('child_process')
 const { resolve } = require('path')
 
-  ; (async () => {
-    const script = resolve(__dirname, '../crawler/video')
-    const child = cp.fork(script, [])
-    let invoke = false
+;(async () => {
+  const script = resolve(__dirname, '../crawler/video')
+  const child = cp.fork(script, [])
+  let invoke = false
 
-    child.on('error', err => {
-      if (invoke) return
-      invoke = true
-      console.log(err)
-    })
+  child.on('error', err => {
+    if (invoke) return
+    invoke = true
+    console.log(err)
+  })
 
-    child.on('exit', code => {
-      if (invoke) return
-      invoke = true
-      let err = code === 0 ? null : new Error('exit code ' + code)
+  child.on('exit', code => {
+    if (invoke) return
+    invoke = true
+    let err = code === 0 ? null : new Error('exit code ' + code)
 
-      console.log(err)
-    })
+    console.log(err)
+  })
 
-    child.on('message', data => {
-      let result = data.result
-      console.log(result)
-    })
-  })()
+  child.on('message', data => {
+    console.log(data)
+  })
+})()
