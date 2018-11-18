e2024af6312ec36c9d7beb5488fcba2d7df63e74
jiaoguanwen <jiaoguanwen@163.com>
2018-11-18 20:32:12 +0800

    add movie data to mongo

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
 const Koa = require('koa')
 const views = require('koa-views')
 const { resolve } = require('path')
-const { connect } = require('./database/init')
+const { connect, initSchemas } = require('./database/init')
+const mongoose = require('mongoose')
 
 ;(async () => {
   await connect()
 
+  initSchemas()
+
   const app = new Koa()
+
+  require('./tasks/movie')
 
   // Add render function to the ctx and some options
   app.use(
--- server/tasks/movie.js
+++ server/tasks/movie.js
@@ -1,27 +1,38 @@
 const cp = require('child_process')
 const { resolve } = require('path')
+const mongoose = require('mongoose')
+const Movie = mongoose.model('Movie')
 
-  ; (async () => {
-    const script = resolve(__dirname, '../crawler/trailer-list')
-    const child = cp.fork(script, [])
-    let invoke = false
+;(async () => {
+  const script = resolve(__dirname, '../crawler/trailer-list')
+  const child = cp.fork(script, [])
+  let invoke = false
 
-    child.on('error', err => {
-      if (invoke) return
-      invoke = true
-      console.log(err)
+  child.on('error', err => {
+    if (invoke) return
+    invoke = true
+    console.log(err)
+  })
+
+  child.on('exit', code => {
+    if (invoke) return
+    invoke = true
+    let err = code === 0 ? null : new Error('exit code ' + code)
+
+    console.log(err)
+  })
+
+  child.on('message', data => {
+    let result = data.result
+    console.log(result)
+    result.forEach(async item => {
+      let movie = await Movie.findOne({
+        doubanId: item.doubanId
+      })
+      if (!movie) {
+        movie = new Movie(item)
+        await movie.save()
+      }
     })
-
-    child.on('exit', code => {
-      if (invoke) return
-      invoke = true
-      let err = code === 0 ? null : new Error('exit code ' + code)
-
-      console.log(err)
-    })
-
-    child.on('message', data => {
-      let result = data.result
-      console.log(result)
-    })
-  })()
+  })
+})()
